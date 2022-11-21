984334fcf35e66ad8c9768e61c53a7d08154a7bc
vineboneto <vineboneto@gmail.com>
2022-11-21 02:01:24 -0300

    feat: add builder to dynamci queries gorm

--- database/gorm.go
+++ database/gorm.go
@@ -2,21 +2,59 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-type StageFluxoPag struct {
-	Id     int
-	Codemp int
-	Codfil int
+type Person struct {
+	FirstName string
+	LastName  string
+	Email     string
 }
 
 type Cliente struct {
 	Codcli int
 	Nomcli string
+}
+
+type Query struct {
+	Q         string
+	ListWhere []string
+}
+
+func Build() *Query {
+	return &Query{}
+
+}
+
+func (q *Query) Select(s string) *Query {
+	q.Q = " SELECT " + s + " "
+	return q
+}
+
+func (q *Query) From(s string) *Query {
+	q.Q = q.Q + " FROM " + s
+	return q
+}
+
+func (q *Query) Where() *Query {
+	q.ListWhere = append(q.ListWhere, " WHERE 1 = 1 ")
+	return q
+}
+
+func (q *Query) And(s string, v any) *Query {
+
+	if v != "" && v != nil {
+		q.ListWhere = append(q.ListWhere, fmt.Sprintf(s, v))
+	}
+	return q
+}
+
+func (q *Query) ToString() string {
+	return q.Q + strings.Join(q.ListWhere, " AND ")
 }
 
 func timeTrack(start time.Time) {
@@ -28,19 +66,24 @@ func main() {
 	defer timeTrack(time.Now())
 
 	db, _ := gorm.Open(postgres.New(postgres.Config{
-		DSN:                  "user=postgres password=1234 dbname=erpweb port=5432 sslmode=disable",
+		DSN:                  "host=localhost user=postgres password=1234 dbname=godemo port=5432 sslmode=disable",
 		PreferSimpleProtocol: true,
 	}), &gorm.Config{})
 
-	var stage StageFluxoPag
+	var persons []Person
 
-	// var stages []StageFluxoPag
+	db.Table("person").Select("*").Where(&Person{FirstName: "Jason"}).Limit(500).Find(&persons)
 
-	db.Select("id, codemp, codfil").Where("id = ?", 280350).Table("stage_fluxo_pag").First(&stage)
+	firstName := "Jason"
+	var lastName string
 
-	// db.Select("*").Table("stage_fluxo_pag").Limit(500).Find(&stages)
+	sql := Build().Where().And("first_name = '%s'", firstName).And("last_name = '%s'", lastName).ToString()
 
-	fmt.Println(stage)
+	fmt.Println(sql)
+
+	// db.Raw(sql).Scan(&persons)
+
+	fmt.Println(persons)
 
 	// db2, _ := gorm.Open(sqlserver.Open("url"), &gorm.Config{})
 
